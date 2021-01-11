ac1f1fc5f5b596c3fbf0549a38c9a23161bcb856
hwookim <gusdn9754@gmail.com>
2021-01-11 21:36:52 +0900

    docs: 웹팩 설정에 대한 해설 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development",
+  mode: "development", // 실행 모드, production, development, none
   entry: {
-    main: "./src/index.js",
+    main: "./src/index.js", // 최상위 스크립트 파일
   },
   output: {
-    path: path.resolve("./dist"),
-    filename: "[name].js",
+    path: path.resolve("./dist"), // 빌드 파일 생성 위치
+    filename: "[name].js", // 빌드 파일명
   },
   module: {
     rules: [
