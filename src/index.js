8eda9d3c590e3228f04b9f42f725c4f4decf1107
hwookim <gusdn9754@gmail.com>
2021-01-12 00:20:43 +0900

    feat: url-loader, file-loader 사용

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import "./index.css";
+import img from "./img/times-circle.png";
+import big_img from "./img/big.jpeg"
 
 console.log("hello world");
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.innerHTML = `
+    <img src="${img}" />
+    <img src="${big_img}" />
+  `;
+})
--- webpack.config.js
+++ webpack.config.js
@@ -18,12 +18,22 @@ module.exports = {
         ], // 사용할 로더 목록
       },
       {
-        test: /\.css$/, // 로더 적용 대상
+        test: /\.css$/,
         use: [
           "style-loader",
           'css-loader',
         ], // 적용되는 순서는 맨 뒤에서부터 앞으로 (css -> style)
-      }
-    ]
-  }
+      },
+      {
+        test: /\.(png|jpg|gif|svg|jpeg)$/,
+        loader: "url-loader",
+        options: {
+          publicPath: "./dist/", // 파일 호출하는 쪽에 파일 위치를 알림
+          name: "[name].[ext]?[hash]", // 이미지 명을 해쉬값을 포함해서 찾도록 함. 해쉬값은 캐쉬 무력화를 할 수 있게 한다.
+          limit: 20000, // 20kb 이하의 파일만 처리, 그 이상은 file-loader를 통해 처리함 => file-loader도 설치되어 있어야 함!
+        },
+      },
+    ],
+  },
 }
+ㅑ
