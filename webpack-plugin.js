26a574200df10c4e6e423c075026ba2a96e04af3
hwookim <gusdn9754@gmail.com>
2021-01-14 00:25:13 +0900

    feat: 빌드된 파일에 배너 추가

--- webpack-plugin.js
+++ webpack-plugin.js
@@ -1,9 +1,27 @@
 class WebpackPlugin {
   apply(compiler) {
     compiler.hooks.done.tap("My Plugin", stats => {
-      console.log("WebpackPlugin: done")
-    })
-  }
+      console.log("MyPlugin: done");
+    });
+
+    // compiler.plugin() 함수로 후처리한다
+    compiler.plugin("emit", (compilation, callback) => {
+      const source = compilation.assets['main.js'].source();
+
+       compilation.assets['main.js'].source = () => {
+        const banner = [
+          '/**',
+          ' * 이것은 BannerPlugin이 처리한 결과입니다.',
+          ' * Build Date: 2020-01-14',
+          ' */',
+          ''
+        ].join('\n');
+        return banner + '\n' + source;
+      }
+
+      callback();
+    });
+  };
 }
 
 module.exports = WebpackPlugin;
