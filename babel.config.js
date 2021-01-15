4f9c1d860a32f9b5752912016cb033cd5ee9ad26
hwookim <gusdn9754@gmail.com>
2021-01-15 19:19:22 +0900

    chore: transform-arrow-functions 플러그인 설치

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   plugins: [
     "@babel/plugin-transform-block-scoping", // const, let => var
+    "@babel/plugin-transform-arrow-functions", // 화살표 함수 변경 '() =>' => 'function()'
   ],
 }
