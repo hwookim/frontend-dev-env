cc08a407b73c9f3cb4a6d9baef2a8b1df5ccc263
hwookim <gusdn9754@gmail.com>
2021-01-19 20:38:58 +0900

    chore: prettier 설정 plugin:prettier/recommended로 통합

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,15 +6,13 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-    "eslint-config-prettier", // eslint와 prettier의 충돌을 방지
+    "plugin:prettier/recommended", // eslint-config-prettier + eslint-plugin-prettier
   ],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["prettier"],
   rules: {
     semi: "error",
-    "prettier/prettier": "error", // prettier 규칙에 어긋나는 문법을 eslint를 통해 오류 표시
   },
 };
